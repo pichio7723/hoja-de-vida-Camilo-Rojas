a15b2a6431d673a5a42528bc8f5c6ba496376f96
Brayan Estiven Rojas Rueda <pichirionix7723@gmail.com>
2026-04-20 18:15:29 -0500

    agrego barra de navegacion

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,1 +1,15 @@
 import {Link} from "react-router-dom";
+
+function Navbar() {
+    return(
+        <nav style={{padding: "10px", backgroundColor: "#2c3e50", color: "white"}}>
+            <ul style={{display: "flex", gap: "20px", listStyle: "none"}}>
+                <li><Link to="/" style={{ color: "white" }}>Home</Link></li>
+                <li><Link to="/perfil" style={{ color: "white" }}>Perfil</Link></li>
+                <li><Link to="/contacto" style={{ color: "white" }}>Contacto</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
