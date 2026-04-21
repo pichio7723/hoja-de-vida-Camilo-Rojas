b754e980ebbf909f5f4c82c72a8b7eee345273e6
Brayan Estiven Rojas Rueda <pichirionix7723@gmail.com>
2026-04-21 13:47:17 -0500

    fix: arreglando barra de navegacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";   // tu página principal
 import Perfil from "./pages/Hobbies"; // crea este archivo
-import Contacto from "./pages/estudios"; // crea este archivo
+import Estudios from "./pages/estudios"; // crea este archivo
 import './App.css';
 import Hobbies from "./pages/Hobbies";
 
@@ -12,8 +12,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/perfil" element={<Hobbies />} />
-        <Route path="/contacto" element={<estudios />} />
+        <Route path="/Hobbies" element={<Hobbies />} />
+        <Route path="/Estudios" element={<Estudios />} />
       </Routes>
     </Router>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
         <nav style={{padding: "10px", backgroundColor: "#2c3e50", color: "white"}}>
             <ul style={{display: "flex", gap: "20px", listStyle: "none"}}>
                 <li><Link to="/" style={{ color: "white" }}>Home</Link></li>
-                <li><Link to="/perfil" style={{ color: "white" }}>Perfil</Link></li>
-                <li><Link to="/contacto" style={{ color: "white" }}>Contacto</Link></li>
+                <li><Link to="/Hobbies" style={{ color: "white" }}>Hobbies</Link></li>
+                <li><Link to="/Estudios" style={{ color: "white" }}>Estudios</Link></li>
             </ul>
         </nav>
     );
