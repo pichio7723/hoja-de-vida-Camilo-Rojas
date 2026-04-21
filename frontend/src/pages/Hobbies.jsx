33e4dada3d0a5856016eba171cba6e5518444923
Stiven <stiven.sana.dev@gmail.com>
2026-04-20 21:19:11 -0500

    refactor: mejora de la estructura Hobbies.jsx

--- frontend/src/pages/Hobbies.jsx
+++ frontend/src/pages/Hobbies.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 
+//importar imágenes
+import gym from "../assets/gym.jpg";
+import programar from "../assets/programacion.jpg";
+import juegos from "../assets/zelda.gif";
+
 function Hobbies() {
+
+  const hobbies = [
+    {
+      nombre: "💪 GYM",
+      imagen: gym,
+      desc: "llevo entrenando 2 años y medio, entreno mas fuerza me gustan los desafios y estoy listo para soportar un peso mayor al promedio que levanto."
+    },
+    {
+      nombre: "💻 Programar",
+      imagen: programar,
+      desc: "Disfruto crear proyectos, he llevado dos años desde mi tecnico aprendiendo nuevas tecnologias, las mas usadas de el mercado desde estos ultimos años hasta las mejores de 2026."
+    },
+    {
+      nombre: "🎮 Videojuegos",
+      imagen: juegos,
+      desc: "me gustan los videosjuegos, fueron mi inspiracion para comenzar a programar estos fueron mi inicio con el software y gracias a ellos he adquirido habilidades como el trabajo en equipo, la comunicacion y la coordinacion."
+    }
+  ];
 
   return (
     <div style={styles.container}>
       <h2 style={styles.title}>Mis Hobbies</h2>
 
       <div style={styles.list}>
+        {hobbies.map((hobby, index) => (
+          <div key={index} style={styles.card}>
+            
+            <h3>{hobby.nombre}</h3>
 
-        <div style={styles.card}>
-          <h3>💪 GYM</h3>
-          <p>llevo entrenando 2 años y medio, entreno mas fuerza me gustan los desafios y estoy listo para soportar un peso mayor al promedio que levanto.</p>
-        </div>
+            <img src={hobby.imagen} style={styles.img} />
 
-        <div style={styles.card}>
-          <h3>💻 Programar</h3>
-          <p>Disfruto crear proyectos, he llevado dos años desde mi tecnico aprendiendo nuevas tecnologias, las mas usadas de el mercado desde estos ultimos años hasta las mejores de 2026.</p>
-        </div>
+            <p>{hobby.desc}</p>
 
-        <div style={styles.card}>
-          <h3>🎮 Videojuegos</h3>
-          <p>me gustan los videosjuegos, fueron mi inspiracion para comenzar a programar estos fueron mi inicio con el software y gracias a ellos he adquirido habilidades como el trabajo en equipo, la comunicacion y la coordinacion.</p>
-        </div>
-
+          </div>
+        ))}
       </div>
     </div>
   );
@@ -30,24 +48,33 @@ function Hobbies() {
 
 const styles = {
   container: {
-    padding: "20px",
+    padding: "50px",
     color: "white",
     textAlign: "center"
   },
+
   title: {
-    marginBottom: "20px"
+    marginBottom: "100px"
   },
+
   list: {
     display: "flex",
-    justifyContent: "center",
-    gap: "20px"
+    flexDirection: "column", // (vertical)
+    alignItems: "center",
+    gap: "60px"
   },
+
   card: {
     background: "#1e293b",
-    padding: "15px",
+    padding: "20px",
     borderRadius: "10px",
-    width: "180px",
-    transition: "0.3s"
+    width: "800px"
+  },
+
+  img: {
+    width: "100%",
+    borderRadius: "10px",
+    margin: "10px 0"
   }
 };
 
