f318d868f6c15c837885606e53e49cde38365ef4
Brayan Estiven Rojas Rueda <pichirionix7723@gmail.com>
2026-04-20 19:49:15 -0500

    fix: arreglando barra de navegacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";   // tu página principal
-//import Perfil from "./pages/hobbyes"; // crea este archivo
-//import Contacto from "./pages/estudios"; // crea este archivo
+import Perfil from "./pages/Hobbies"; // crea este archivo
+import Contacto from "./pages/estudios"; // crea este archivo
 import './App.css';
+import Hobbies from "./pages/Hobbies";
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/perfil" element={<Hobbies />} />
+        <Route path="/contacto" element={<estudios />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/estudios.jsx
@@ -0,0 +1,7 @@
+function estudios() {
+    return (
+        <h1>estudios</h1>
+    );
+}
+
+export default estudios;
