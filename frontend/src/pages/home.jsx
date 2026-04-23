78a23e6c5ae66b80de8636947ecb205ef5b40643
Brayan Estiven Rojas Rueda <pichirionix7723@gmail.com>
2026-04-22 22:51:58 -0500

    feat: cambie estilos de home.jsx y añado boton de cv

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,25 +2,35 @@ import Perfil from "../assets/sonic.gif"
 import { Document, Page } from 'react-pdf';
 import HojaDeVida from "../components/hoja_de_vida";
 import Tecnologias from "../assets/tecnologias.png";
-
+import { useState } from "react";
 
 function Home() {
+  const [mostrarCV, setMostrarCV] = useState(false); //definicion de elementos del boton
   return (
     <div className="page-layout">
       <div className="home-container">
-        <h1 className="home-title">❗Hola bienvenido a mi hoja de Vida 😉</h1>
+        <h1 className="home-title">Hola bienvenido a mi hoja de Vida </h1>
         <br/><br/>
 
         <article className="home-article">
           <section className="cv-section">
-            <h2>🪪Hoja de Vida</h2>
-            <HojaDeVida/>
+            <h2>★ Hoja de Vida</h2>
+            <button 
+              className="toggle-btn"
+              onClick={() => setMostrarCV(!mostrarCV)}
+            >
+              {mostrarCV ? "Ocultar CV" : "Mostrar CV"}
+            </button>
+
+            {/* Render condicional */}
+            {mostrarCV && <HojaDeVida />}
           </section>
 
           <section className="about-section">
-            <h2>👤Un poco Sobre mi</h2>
+            <h2>★ Un poco Sobre mi</h2>
             <img src={Perfil} width="500px" height="450px" className="profile-img"/>
             <p className="about-text">
+              <br/>
               ✦ Soy un joven apasionado por la tecnologia y los videojuegos, busco mejorar cada dia como persona y como profesional adquiriendo mis conocimientos en desarrollo de software actualizados y aprendiendo las mejores tecnologias de el mercado, Tengo buenas habilidades blandas, se trabajar en equipo y deseo aportar a tu empresa con mi conocimiento.
             </p>
           </section>
