99ff83fb1afc0bb173d451d7e7d9b24dfece3145
Stiven <stiven.sana.dev@gmail.com>
2026-04-24 00:00:24 -0500

    feat: agrego array de estudios.jsx

--- frontend/src/pages/estudios.jsx
+++ frontend/src/pages/estudios.jsx
@@ -1,19 +1,52 @@
+import React from "react";
+
 import FondoNegro from "../components/fondo_negro";
 
-function estudios() {
-    return (
+  function Estudios () {
 
-        <FondoNegro> 
-        <h1>estudios</h1>
+    const Estudios = [
 
 
+        {
 
+            titulo:"Bachiller",
+            lugar:"Ofelia Uribe De Acosta",
+            descripcion: "culmine mi bachillerato con formacion academica general",
+            año: "2025"
 
+        }, 
+        {
 
+             titulo:"Tecnico en Programacion de software",
+            lugar:"Ofelia Uribe De Acosta",
+            descripcion: "culmine mi Tenico con formacion academica general",
+            año: "2025"
 
+        }, 
+        {
 
-        </FondoNegro>
-    );
+             titulo:"Tecnologo en formacion de Programacion de software",
+            lugar:"SENA - Centro de Gestión de Mercados, Logística y Tecnologías de la Información - Regional Distrito Capital",
+            descripcion: "Estoy en formacion de mi tecnologo, aprendiendo cada dia",
+            año: "Finalizacion 2027"
+
+        }, 
+        {
+
+             titulo:"Certificado de Ingles",
+            lugar:"Ofelia Uribe De Acosta",
+            descripcion: "Culmine mis estudios con el mejor Ingles de mi colegio",
+            año: "2025"
+
+        }
+
+    ]
 }
 
-export default estudios;
+export default Estudios;
+
+
+
+
+
+
