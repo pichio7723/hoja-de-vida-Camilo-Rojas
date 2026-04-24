d2c17896ea61fdc569ac1e1efa4b63b6a526dafd
Stiven <stiven.sana.dev@gmail.com>
2026-04-23 23:44:02 -0500

    trayendo cambios

--- frontend/src/pages/estudios.jsx
+++ frontend/src/pages/estudios.jsx
@@ -1,6 +1,18 @@
+import FondoNegro from "../components/fondo_negro";
+
 function estudios() {
     return (
+
+        <FondoNegro> 
         <h1>estudios</h1>
+
+
+
+
+
+
+
+        </FondoNegro>
     );
 }
 
